7edd7d633e288218fd391b84d30099bed0bb3929
Amit Kumar <amitnitsri25@gmail.com>
2020-12-25 18:05:41 +0530

    UC2 : Created Payroll - Form Page to Add Employee

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import PayrollForm from './components/payroll-form/payroll-form';
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route, Redirect
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      Hello Folks !!
+      <Router>
+        <Switch>
+          <Route exact path="">
+            <PayrollForm />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
